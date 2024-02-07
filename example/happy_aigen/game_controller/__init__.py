d2c64aed5490753f0bd270c8c973d65dd52f436a
aaronyuai <c@crynux.ai>
2024-02-06 20:28:05 -0800

    Move happy aigen to new repo

--- example/happy_aigen/game_controller/__init__.py
+++
@@ -1,7 +0,0 @@
-from game_controller import admin
-from game_controller import user
-
-__all__ = [
-  "admin",
-  "user",
-]
--- example/happy_aigen/pixel_enigma/__init__.py
+++
@@ -1,5 +0,0 @@
-from pixel_enigma.commands import add_commands
-
-__all__ = [
-  "add_commands",
-]
